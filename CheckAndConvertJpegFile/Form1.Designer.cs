2566f23510be0bcac626f5c35ccb580769d7613c
FREEWING-JP <freewing.jp@gmail.com>
2020-11-21 13:06:51 +0900

    add Project Source

---
+++ CheckAndConvertJpegFile/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace CheckAndConvertJpegFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(5, 50);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(345, 30);
+            this.label2.TabIndex = 4;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(5, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(345, 45);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Location = new System.Drawing.Point(140, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "http://www.neko.ne.jp/~freewing/";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.checkBox1.Location = new System.Drawing.Point(5, 80);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(198, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Accept .bmp, .gif, .tif, .png";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.checkBox2.Location = new System.Drawing.Point(210, 80);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(140, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Force Over Write";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 112);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "CheckAndConvertJpegFile";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.DragLeave += new System.EventHandler(this.Form1_DragLeave);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ CheckAndConvertJpegFile/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CheckAndConvertJpegFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox1.Text = "Convert Progressive JPEG to Baseline JPEG\r\n for HackBGRT MULTI\r\nhttps://github.com/FREEWING-JP/HackBGRT";
+            textBox1.Select(0, 0);
+            label2.Text = "Drag and Drop JPEG file here";
+        }
+
+        private void CheckAndConvertJpegFile(string inputFilePath)
+        {
+            label2.Text = "";
+
+            if (CheckFileExtensionIsJpeg(inputFilePath))
+            {
+
+                // [SOI 0xFFD8]-[SOF 0xFFCx]-[SOS 0xFFDA]
+                // Search SOF0 Baseline Standard 0xFFC0 or Not
+                // Search SOF2 Progressive 0xFFC2
+                byte[] rbuff = ReadByteFile(inputFilePath);
+                bool isBaseline = false;
+                int seq = 0;
+                int pos = 0;
+                while (pos < rbuff.Length)
+                {
+                    byte b = rbuff[pos++];
+                    if (b != (byte)0xFF) continue;
+
+                    if (pos >= rbuff.Length)
+                        break;
+
+                    b = rbuff[pos++];
+                    if (seq == 0)
+                    {
+                        // SOI ?
+                        if (b != (byte)0xD8) continue;
+                        ++seq;
+                        continue;
+                    }
+
+                    if (seq == 1)
+                    {
+                        // SOF ?
+                        if ((b & (byte)0xF0) != (byte)0xC0) continue;
+
+                        // SOF0 ?
+                        if (b == (byte)0xC0)
+                        {
+                            isBaseline = true;
+                        }
+                        break;
+                    }
+
+                    if (seq == 1)
+                    {
+                        // SOS ?
+                        if (b != (byte)0xDA) continue;
+
+                        label2.Text = "Invalid JPEG file";
+                        return;
+                    }
+                }
+
+                if (isBaseline)
+                {
+                    label2.Text = "Is Baseline JPEG";
+                    return;
+                }
+            }
+
+            string fileName = Path.GetFileNameWithoutExtension(inputFilePath);
+            string fileExt = ".jpg";
+            string filePath = Path.GetDirectoryName(inputFilePath);
+
+            // Add Prefix "bl_"
+            string outFilePath = Path.Combine(filePath, "bl_" + fileName + fileExt);
+            if (File.Exists(outFilePath) && !checkBox2.Checked)
+            {
+                label2.Text = "Exists Same Filename";
+                return;
+            }
+
+            try
+            {
+                using (Image source = Image.FromFile(inputFilePath))
+                {
+                    ImageCodecInfo codec = ImageCodecInfo.GetImageEncoders().First(c => c.MimeType == "image/jpeg");
+
+                    // https://developers.google.com/speed/docs/insights/OptimizeImages
+                    // long GOOGLE_OPTIMIZE_JPEG_QUALUTY = 85L;
+                    long JPEG_QUALUTY = 95L;
+                    EncoderParameters parameters = new EncoderParameters(3);
+                    parameters.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, JPEG_QUALUTY);
+                    parameters.Param[1] = new EncoderParameter(System.Drawing.Imaging.Encoder.ScanMethod, (int)EncoderValue.ScanMethodNonInterlaced);
+                    parameters.Param[2] = new EncoderParameter(System.Drawing.Imaging.Encoder.ScanMethod, (int)EncoderValue.RenderNonProgressive);
+                    // parameters.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 100L);
+                    // parameters.Param[1] = new EncoderParameter(System.Drawing.Imaging.Encoder.ScanMethod, (int)EncoderValue.ScanMethodInterlaced);
+                    // parameters.Param[2] = new EncoderParameter(System.Drawing.Imaging.Encoder.RenderMethod, (int)EncoderValue.RenderProgressive);
+
+                    source.Save(outFilePath, codec, parameters);
+
+                    label2.Text = "Convert OK";
+                }
+            }
+            catch (Exception ex)
+            {
+                label2.Text = "Convert ERROR";
+            }
+        }
+
+        private byte[] ReadByteFile(string filePath)
+        {
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                // long readLength = Math.Min(fs.Length, 2048L);
+                long readLength = fs.Length;
+                byte[] buffer = new byte[readLength];
+                fs.Read(buffer, 0, buffer.Length);
+                return buffer;
+            }
+        }
+        private bool CheckFileExtensionIsJpeg(string fileName)
+        {
+            string ext = Path.GetExtension(fileName); // .ToLower(CultureInfo.InvariantCulture);
+
+            string[] JPEG = new string[] { ".jpg", ".jpeg" };
+            if (JPEG.Contains(ext, StringComparer.OrdinalIgnoreCase))
+                return true;
+
+            return false;
+        }
+
+        private bool CheckFileExtensionIsOthers(string fileName)
+        {
+            string ext = Path.GetExtension(fileName); // .ToLower(CultureInfo.InvariantCulture);
+
+            string[] OTHER_IMAGES = new string[] { ".bmp", ".gif", ".tif", ".tiff", ".png" };
+            if (OTHER_IMAGES.Contains(ext, StringComparer.OrdinalIgnoreCase))
+                return true;
+
+            return false;
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.None;
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+                return;
+
+            string[] fileNames =
+                (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            if (fileNames.Length != 1)
+                return;
+
+            string fileName = fileNames[0];
+            if (CheckFileExtensionIsJpeg(fileName))
+            {
+            }
+            else if (CheckFileExtensionIsOthers(fileName) && checkBox1.Checked)
+            {
+            }
+            else
+            {
+                label2.Text = "Not Support";
+                return;
+            }
+
+            e.Effect = DragDropEffects.Copy;
+            textBox1.Text = fileName;
+            label2.Text = "Drop Here !";
+        }
+
+        private void Form1_DragLeave(object sender, EventArgs e)
+        {
+            label2.Text = "";
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] fileNames =
+                (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            string fileName = fileNames[0];
+
+            CheckAndConvertJpegFile(fileName);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.neko.ne.jp/~freewing/");
+        }
+    }
+}
